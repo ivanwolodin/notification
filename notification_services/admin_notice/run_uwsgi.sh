03c25cc5c540724817d9d520981b93914e699be8
Smoooky <smoooky2012@gmail.com>
2024-03-31 07:45:58 +0300

    Add: django admin panel

--- notification_services/admin_notice/run_uwsgi.sh
+++ notification_services/admin_notice/run_uwsgi.sh
@@ -1,19 +1,9 @@
 #!/usr/bin/env bash
 
-echo "Waiting for postgres..."
-
-#while ! nc -z $DB_HOST $DB_PORT; do
-#  sleep 0.1
-#done
-
-sleep 60
-
-echo "PostgreSQL started"
-
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
 python3 manage.py migrate sessions
-python3 manage.py createsuperuser --no-input || true
+python3 manage.py createsuperuser --username root --email email@email.com --no-input || true
 python3 manage.py collectstatic --noinput
 
 #python3 manage.py compilemessages -l en -l ru
@@ -22,4 +12,4 @@ set -e
 chown www-data:www-data /var/log
 
 
-#uwsgi --strict --ini uwsgi.ini
+uwsgi --strict --ini uwsgi/uwsgi.ini
