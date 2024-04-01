2469df23f03623724d9ee6516fdac561c2e29813
Ivan Volodin <ivanwolodin@gmail.com>
2024-04-01 21:27:56 +0500

    Add: setting config file

---
+++ rabbitmq/consumer/settings.py
@@ -0,0 +1,14 @@
+import os
+
+
+class Settings:
+
+    RABBITMQ_DEFAULT_USER = os.getenv('RABBITMQ_DEFAULT_USER', 'guest')
+    RABBITMQ_DEFAULT_PASS = os.getenv('RABBITMQ_DEFAULT_PASS', 'guest')
+    RABBITMQ_CONTAINER_NAME = os.getenv('RABBITMQ_CONTAINER_NAME', 'rabbit_queue')
+    RABBITMQ_QUEUE_NAME = os.getenv('RABBITMQ_QUEUE_NAME', 'email_queue')
+
+    SMTP_USER = os.getenv('SMTP_USER'),
+
+
+settings = Settings()
